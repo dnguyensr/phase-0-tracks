41be9f1b7269292ae64d730ca78a7c5bfb3dcc5e
Daniel Nguyen <danielnguyensr@gmail.com>
2016-03-29 02:09:36 -0700

    Update method-detective

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,42 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# “iNvEsTiGaTiOn”.<???>
+p "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# “zom”.<???>
+p "zom".insert(1, 'o')
+p "zom".insert(2, 'o')
 # => “zoom”
 
-# “enhance”.<???>
+p "enhance".center(15)
 #=> “    enhance    ”
 
-# “Stop! You’re under arrest!”.<???>
+p "Stop! You’re under arrest!".upcase
 # => “STOP! YOU’RE UNDER ARREST!”
 
-# “the usual”.<???>
+p "the usual" + " suspects"
+p "the usual" << " suspects"
 #=> “the usual suspects”
 
-#“ suspects”.<???>
+p " suspects".insert(0, 'the usual')
+p " suspects".prepend("the usual")
 #=> “the usual suspects”
 
-# “The case of the disappearing last letter”.<???>
+p "The case of the disappearing last letter".chop
+p "The case of the disappearing last letter".slice!(0..38)
 # => “The case of the disappearing last lette”
 
-# “The mystery of the missing first letter”.<???>
+p "The mystery of the missing first letter".slice!(1..38)
 # => “he mystery of the missing first letter”
 
-# “Elementary  ,    my   dear      Watson!”.<???>""
+p "Elementary  ,    my   dear      Watson!".squeeze!
 # => “Elementary, my dear Watson!”
 
-#“z”.<???>
-# => 122 
+p "z".bytes.join.to_i
+"z".each_byte {|i| puts i}
+"z".each_codepoint {|i| puts i}
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
-#“How many times does the letter ‘a’ appear in this string?”.<???>
+p "How many times does the letter ‘a’ appear in this string?".count "a"
 #=> 4
