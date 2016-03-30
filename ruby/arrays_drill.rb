3c3d43071ce2c92cb444801dd404da472db26def
Daniel Nguyen <danielnguyensr@gmail.com>
2016-03-29 17:59:05 -0700

    Update arrays_drill

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,4 +1,4 @@
-def ourmethod(item1, item2, item3)
+def build_array(item1, item2, item3)
   arr = [item1, item2, item3]
 end
 
@@ -6,31 +6,36 @@ def add_to_array(arr, item)
   arr << item
 end
 
-#drill = []
-#p drill
-#drill << 1 << 2 << 3 << 4 << 5
-#p drill
-#drill.delete_at(2)
-#p drill
-#drill.insert(2, "three")
-#p drill
-#drill.shift
-#p drill
-#
-#if drill.index(6) == nil
-# our_index = "(nil)"
-#else
-# our_index = drill.index(6)
-#end
-#
-#p "The index of FixNum 6 is: #{our_index}"
-#
-#drill_two = [6, 7, 8]
-#
-#combined_array = drill + drill_two
-#p combined_array
+drill = []
+p drill
+
+drill << 1 << 2 << 3 << 4 << 5
+p drill
+
+drill.delete_at(2)
+p drill
+
+drill.insert(2, "three")
+p drill
+
+drill.shift
+p drill
+
+if drill.index(6) == nil
+ our_index = "(nil)"
+else
+ our_index = drill.index(6)
+end
+
+p "The index of FixNum 6 is: #{our_index}"
+
+drill_two = [6, 7, 8]
+
+combined_array = drill + drill_two
+p combined_array
 
 
-p ourmethod("apple", "orange", "banana")
+p build_array(1, "two", nil)
 p add_to_array(["one", "two", "three"], 5)
+p add_to_array(["a","b","c", 1, 2], 3)
 p add_to_array([],5)
