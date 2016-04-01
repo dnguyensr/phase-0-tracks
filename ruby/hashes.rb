fbc542bd0bc7fdd53c00fd1eaa9c9ae235829e37
Daniel Nguyen <danielnguyensr@gmail.com>
2016-03-31 17:30:08 -0700

    Update hashes

---
+++ ruby/hashes.rb
@@ -0,0 +1,37 @@
+#prompt and collect for client's name, age, number of children, decor theme
+#convert to correct datatype
+#push to hash
+#print hash at end
+#Prompt for update
+#skip if 'none'
+
+puts "What is the client's first name?"
+first_name = gets.chomp.to_s
+puts "What is the client's last name?"
+last_name = gets.chomp.to_s
+puts "Is the client a first time customer? (yes/no)"
+first_visit = gets.chomp.to_s
+if first_visit == "yes"
+  loyalcust = true
+else loyalcust = false
+end
+puts "What is the client's age?"
+age = gets.chomp.to_i
+puts "How many children does the client have?"
+numchildren = gets.chomp.to_i
+puts "What decor theme would you like?"
+decor_theme = gets.chomp.to_s
+
+cust_info = {
+  first_name: first_name,
+  last_name: last_name,
+  loyalcust: loyalcust,
+  age: age,
+  numchildren: numchildren,
+  decor_theme: decor_theme
+}
+
+puts cust_info
+
+puts "Would you like to update or edit any information?"
+fixhash = gets.chomp
