0988dd7316ce1b71c0228485630f758e283afc49
Daniel Nguyen <danielnguyensr@gmail.com>
2016-04-07 03:05:00 -0700

    Update puppy_methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,48 +1,48 @@
-#class Puppy
-#
-#  def fetch(toy)
-#    puts "I brought back the #{toy}!"
-#    toy
-#  end
-#
-#  def speak(time)
-#    time.to_i
-#    time.times do puts "Woof!"
-#    end
-#
-#  end
-#
-#  def roll_over
-#    puts "*rolls over*"
-#  end
-#
-#  def dog_years(years)
-#    years.to_f
-#
-#      if years < 2
-#        years = years * 10
-#      else years = years * 4 + 20
-#      end
-#
-#    puts "The puppy is #{years} dog years ld"
-#  end
-#
-#  def dress(clothes)
-#    puts "The puppy is wearing #{clothes}."
-#  end
-#
-#  def initialize
-#    puts "Initializing new puppy instance..."
-#  end
-#
-#end
-#
-#spot = Puppy.new
-#spot.fetch("bone")
-#spot.speak(2)
-#spot.roll_over
-#spot.dog_years(2.5)
-#spot.dress("a hat")
+class Puppy
+
+  def fetch(toy)
+    puts "I brought back the #{toy}!"
+    toy
+  end
+
+  def speak(time)
+    time.to_i
+    time.times do puts "Woof!"
+    end
+
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(years)
+    years.to_f
+
+      if years < 2
+        years = years * 10
+      else years = years * 4 + 20
+      end
+
+    puts "The puppy is #{years} dog years ld"
+  end
+
+  def dress(clothes)
+    puts "The puppy is wearing #{clothes}."
+  end
+
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
+
+end
+
+spot = Puppy.new
+spot.fetch("bone")
+spot.speak(2)
+spot.roll_over
+spot.dog_years(2.5)
+spot.dress("a hat")
 
 
 class Students
