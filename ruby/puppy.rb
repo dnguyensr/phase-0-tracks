b219cffcc2c8c3791d373ded70c6915607f4ba54
Daniel Nguyen <danielnguyensr@gmail.com>
2016-04-07 23:48:18 -0700

    Update puppy with IRB examples

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -13,3 +13,17 @@ derps
 memes
 makes faces
 =end
+
+class Puppy
+end
+
+Puppy.methods
+duchess = Puppy.new
+fido  = Puppy.new
+spot = Puppy.new
+
+p spot.class
+p duchess == fido
+p fido.instance_of?(Array)
+p fido.instance_of?(Puppy)
+spot.play_dead
