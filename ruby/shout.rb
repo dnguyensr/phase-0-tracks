6367c0e7804a615e04aa444f5713a743c45b83a5
Daniel Nguyen <danielnguyensr@gmail.com>
2016-04-08 18:11:33 -0700

    Update shout.rb and flight.rb with mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,1 +1,25 @@
-#initial
+module Shout
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yell_happily(words)
+    words + "!!!! :)"
+  end
+#  p self.yell_angrily("So angry")
+#  p self.yell_happily("So happy")
+end
+
+class Heckler
+  include Shout
+end
+
+class Fan
+  include Shout
+end
+
+bob = Heckler.new
+p bob.yell_angrily("Tone down for what")
+
+bill = Fan.new
+p bill.yell_happily("We won")
