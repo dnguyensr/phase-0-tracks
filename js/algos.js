7f64f7936596155a8c18814e805d337515cbca59
Daniel Nguyen <danielnguyensr@gmail.com>
2016-04-17 21:40:23 -0700

    Update algos with Compare_pair function

--- js/algos.js
+++ js/algos.js
@@ -14,15 +14,35 @@ function longest(array) {
   }
   console.log(highlength);
 }
+//Release 1: Find a Key-Value Match
+//Declare new function that takes two objects
+  //IF age OR
+  //Store obects into hash
+  //For each object, check if either
 
+function Compare_pair(pairone, pairtwo) {
+  if ((pairone.name == pairtwo.name) || (pairone.age == pairtwo.age)) {
+    return true
+  }  else {
+    return false
+  }
+}
+
+//Release 0 driver code
 var arr = ["long phrase", "even longer phrase", "longer phrase", "longest phrase"];
 var arr3 = ["one", "two", "onethousand", "three", "seventeen"];
 
 longest(arr);
 longest(arr3);
 
-//Release 1: Find a Key-Value Match
-//Declare new function that takes two objects
-  //Declare new variable as a hash
-  //Store obects into hash
-  //For each object, check if either
+//Release 1 driver code
+var Steve = {name: "Steven", age: 54};
+var Tami =  {name: "Tamir"
+, age: 54};
+var Bob = {name: "Bob", age: 5};
+var Bob2 = {name: "Bob", age: 7}
+
+console.log(Compare_pair(Steve, Tami));
+console.log(Compare_pair(Bob, Bob2));
+console.log(Compare_pair(Steve, Bob));
+//Release 2 driver code
