6df43021bdae9b63183257b9c477000bc07d9134
Daniel Nguyen <danielnguyensr@gmail.com>
2016-04-30 17:14:40 -0700

    Update index, script, style with eventlistener

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,4 +1,8 @@
-function addTears() {
-  var photo = document.getElementbyID("tears")
-  photo.style.display = "block"
+console.log("The script is working!")
+
+function tears(){
+  var photo = document.getElementById("tears");
+  photo.style.display = "block";
 }
+
+window.setTimeout(tears, 2000);
